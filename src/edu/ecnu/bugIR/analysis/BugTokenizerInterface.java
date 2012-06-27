902581c4687fa39c914cc52ae9b1d6ecda7d5795
songly <leyisong@yahoo.com>
2012-06-27 20:05:46 +0800

    Initial...

---
+++ src/edu/ecnu/bugIR/analysis/BugTokenizerInterface.java
@@ -0,0 +1,11 @@
+package edu.ecnu.bugIR.analysis;
+
+/** 
+ * @description: Preprocess bug reports by Standard IR techniques:tokenize
+ * @author: "Song Leyi"  2012-6-27
+ * @version: 1.0 
+ * @modify: 
+ */
+public class BugTokenizerInterface {
+
+}
